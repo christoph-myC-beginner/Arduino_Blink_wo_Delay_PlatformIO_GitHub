625fe25b2a4f344a404fd411535ec708e3b98fcf
christoph-myC-beginner <christoph.schumachers@gmx.de>
2021-12-25 10:05:40 +0100

    new function

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 int toggleInterval = 200; //Zeit, die die LED an bzw aus ist in Millisekunden
 int msgInterval = 1000;
 
+bool testKeepFlag = 0;
+
 bool keepFlag = 0;
 unsigned long keepHmillis;
 
@@ -15,13 +17,14 @@ unsigned long keepHmillis;
 void noDelayBlink(byte pin, int blinkZeit );
 void serialMsg(int msgZeit);
 void keepPinHighForTime(int keepHTime, int pin);
+bool keepAnyFlagHighForTime(int keepItTime, bool anyFlag);
 void simplePWM(int pin, int cycleOn, int cycleFull);
 
 //////////////////////////////////////////////////////
 // Setup fuer die LED und Serielle Ausgabe
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT); //LED_BUILTIN ist keyword fÃƒÂ¼r die LED auf dem Board, bei Arduino an Pin 13
+  pinMode(LED_BUILTIN, OUTPUT); //LED_BUILTIN ist keyword fuer die LED auf dem Board, bei Arduino an Pin 13
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
@@ -34,11 +37,18 @@ void setup() {
 void loop() {
   noDelayBlink(LED_BUILTIN, toggleInterval);
   serialMsg(msgInterval);
-  keepPinHighForTime(200, 12);
+  keepPinHighForTime(1000, 12);
   if (digitalRead(8) == HIGH) {
     //Serial.println("8 ist HIGH");
     keepFlag = 0;
+    testKeepFlag = 1;
   }
+  else {
+    testKeepFlag = 0;
+  }
+  testKeepFlag = keepAnyFlagHighForTime(2000, testKeepFlag);
+  digitalWrite(11, testKeepFlag);
+
 }
 //////////////////////////////////////////////////////
 //////////////////////////////////////////////////////
@@ -75,6 +85,25 @@ void keepPinHighForTime(int keepHTime, int pin) {
   else {//if (millis() - keepHmillis > keepHTime ODER done Flag == 1)
     digitalWrite(pin, LOW);
     doneFlag = 1;
+  }
+}
+
+bool keepAnyFlagHighForTime(int keepItTime, bool anyFlag) {
+  static unsigned long keepFlagMillis;
+  static bool doneFlag;
+  if (anyFlag == 1 && doneFlag == 0) {
+    keepFlagMillis = millis();
+    doneFlag = 1; //set the millis()
+  }
+  else if (anyFlag == 0) {
+    return 0;
+  }
+  if (millis() - keepFlagMillis < keepItTime) {
+    return 1;
+  }
+  else { //if (millis() - keepFlagmillis > keepItTime ODER anyFlag == 0)
+    doneFlag = 0;
+    return 0;
   }
 }
 
